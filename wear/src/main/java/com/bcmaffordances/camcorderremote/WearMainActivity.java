839aa565d497343d540520235ecd681d215b2605
brian mullins <bmul@users.noreply.github.com>
2014-10-26 14:22:26 -0400

    Initial commit

---
+++ wear/src/main/java/com/bcmaffordances/camcorderremote/WearMainActivity.java
@@ -0,0 +1,24 @@
+package com.bcmaffordances.camcorderremote;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+public class WearMainActivity extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wear_main);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+    }
+}
