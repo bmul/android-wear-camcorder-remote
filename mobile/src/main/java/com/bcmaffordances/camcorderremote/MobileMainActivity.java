4403ec51200aaaa0f119b65676d1acc4a36b656d
brian mullins <bmul@users.noreply.github.com>
2014-11-05 19:15:11 -0500

    Recording functionality now working on mobile app.

--- mobile/src/main/java/com/bcmaffordances/camcorderremote/MobileMainActivity.java
+++ mobile/src/main/java/com/bcmaffordances/camcorderremote/MobileMainActivity.java
@@ -1,20 +1,11 @@
 package com.bcmaffordances.camcorderremote;
 
 import android.app.Activity;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
-import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import com.bcmaffordances.wearableconnector.CamcorderRemoteConstants;
-import com.bcmaffordances.wearableconnector.WearableConnector;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
 
 /**
  * Mobile main activity
@@ -22,88 +13,15 @@ import com.google.android.gms.common.api.GoogleApiClient;
 public class MobileMainActivity extends Activity {
 
     private static final String TAG = "MobileMainActivity";
-    private WearableConnector mWearableConnector;
-    private BroadcastReceiver mLocalMessageReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_mobile_main);
-
-        // Setup wearable connection callbacks
-        GoogleApiClient.OnConnectionFailedListener wearableConnectionFailedListener = new GoogleApiClient.OnConnectionFailedListener() {
-            @Override
-            public void onConnectionFailed(ConnectionResult result) {
-                Log.d(TAG, "onConnectionFailed: " + result);
-            }
-        };
-        GoogleApiClient.ConnectionCallbacks wearableConnectionCallbacks = new GoogleApiClient.ConnectionCallbacks() {
-            @Override
-            public void onConnected(Bundle connectionHint) {
-                Log.d(TAG, "onConnected: " + connectionHint);
-                String message = "Hello wearable!";
-                mWearableConnector.sendMessage(message);
-            }
-            @Override
-            public void onConnectionSuspended(int cause) {
-                Log.d(TAG, "onConnectionSuspended: " + cause);
-            }
-        };
-
-        mWearableConnector = new WearableConnector(
-                this,
-                wearableConnectionCallbacks,
-                wearableConnectionFailedListener);
-
-        // Register a local broadcast receiver to handle messages that
-        // have been received by the wearable message listening service.
-        mLocalMessageReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                String message = intent.getStringExtra(CamcorderRemoteConstants.MESSAGE_INTENT_EXTRA);
-                Log.d(TAG, "Message received from wearable: " + message);
-                if (message.equals(CamcorderRemoteConstants.REQUEST_RECORD)) {
-                    mWearableConnector.sendMessage(CamcorderRemoteConstants.RESPONSE_RECORDING);
-                }
-                else if(message.equals(CamcorderRemoteConstants.REQUEST_RESUME)) {
-                    mWearableConnector.sendMessage(CamcorderRemoteConstants.RESPONSE_RESUMED);
-                }
-                else if(message.equals(CamcorderRemoteConstants.REQUEST_PAUSE)) {
-                    mWearableConnector.sendMessage(CamcorderRemoteConstants.RESPONSE_PAUSED);
-                }
-                else if(message.equals(CamcorderRemoteConstants.REQUEST_STOP)) {
-                    mWearableConnector.sendMessage(CamcorderRemoteConstants.RESPONSE_STOPPED);
-                }
-
-            }
-        };
-        IntentFilter messageFilter = new IntentFilter(Intent.ACTION_SEND);
-        LocalBroadcastManager.getInstance(this).registerReceiver(mLocalMessageReceiver, messageFilter);
-
+        Intent startVideoCaptureActivity = new Intent(MobileMainActivity.this, VideoCaptureActivity.class);
+        startActivityForResult(startVideoCaptureActivity, 123);
     }
-
-    @Override
-    protected void onResume() {
-        Log.d(TAG, "onResume");
-        super.onResume();
-        mWearableConnector.connect();
-    }
-
-    @Override
-    protected void onStop() {
-        Log.d(TAG, "onStop");
-        mWearableConnector.disconnect();
-        super.onStop();
-    }
-
-    @Override
-    protected void onDestroy() {
-        Log.d(TAG, "onDestroy");
-        LocalBroadcastManager.getInstance(this).unregisterReceiver(mLocalMessageReceiver);
-        super.onDestroy();
-    }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- mobile/src/main/java/com/bcmaffordances/camcorderremote/VideoCaptureActivity.java
+++ mobile/src/main/java/com/bcmaffordances/camcorderremote/VideoCaptureActivity.java
@@ -1,8 +1,12 @@
 package com.bcmaffordances.camcorderremote;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.OrientationEventListener;
@@ -21,6 +25,10 @@ import com.bcmaffordances.camcorderremote.state.StoppedRecordingState;
 import com.bcmaffordances.camcorderremote.video.InitCameraListener;
 import com.bcmaffordances.camcorderremote.video.ReleaseCameraListener;
 import com.bcmaffordances.camcorderremote.video.VideoRecorder;
+import com.bcmaffordances.wearableconnector.CamcorderRemoteConstants;
+import com.bcmaffordances.wearableconnector.WearableConnector;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 /**
  * Created by bmullins on 11/5/14.
@@ -30,12 +38,17 @@ public class VideoCaptureActivity extends Activity {
     private static final String TAG = "VideoCaptureActivity";
 
     private Activity mActivity;
+
+    private WearableConnector mWearableConnector;
+    private BroadcastReceiver mLocalMessageReceiver;
+
     private VideoRecorder mVideoRecorder;
     private RecordingStateContext mRecordingStateContext;
     private FrameLayout mCameraPreviewFrame;
     private VideoCaptureOrientationEventListener mOrientationListener;
     // The degrees of the device rotated clockwise from its natural orientation.
     private int mLastRawOrientation = OrientationEventListener.ORIENTATION_UNKNOWN;
+
 
     @Override
     public void onCreate(Bundle bundle) {
@@ -48,31 +61,14 @@ public class VideoCaptureActivity extends Activity {
         mCameraPreviewFrame = (FrameLayout)super.findViewById(R.id.camera_preview);
 
         mActivity = this;
-        mOrientationListener = new VideoCaptureOrientationEventListener(this);
-
-        mRecordingStateContext = new RecordingStateContext(mActivity);
-        mRecordingStateContext.updateDisplayedButtons();
-
-        mVideoRecorder = new VideoRecorder();
-        mVideoRecorder.setOnInitCameraListener(new InitCameraListener(){
-            @Override
-            public void onInit() {
-                mCameraPreviewFrame.addView(mVideoRecorder.getCameraPreview(), 0);
-                mRecordingStateContext.changeState(new ReadyRecordingState(mActivity));
-                mRecordingStateContext.updateDisplayedButtons();
-            }
-        });
-        mVideoRecorder.setOnReleaseCameraListener(new ReleaseCameraListener(){
-            @Override
-            public void onRelease() {
-                mCameraPreviewFrame.removeView(mVideoRecorder.getCameraPreview());
-            }
-        });
+        initWearableConnector();
+        initVideoRecorder();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        mWearableConnector.connect();
         mVideoRecorder.init(this);
     }
 
@@ -81,6 +77,19 @@ public class VideoCaptureActivity extends Activity {
         super.onPause();
         mOrientationListener.disable();
         mVideoRecorder.release();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mWearableConnector.disconnect();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy");
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mLocalMessageReceiver);
+        super.onDestroy();
     }
 
     // gets called by the button press
@@ -139,9 +148,86 @@ public class VideoCaptureActivity extends Activity {
             // create new layout with the current orientation
             Log.d(TAG, "Redrawing view due to orientation change");
             ViewGroup appRoot = (ViewGroup) findViewById(R.id.preview_root);
-            appRoot.removeView(mCameraPreviewFrame);
+            //appRoot.removeView(mCameraPreviewFrame);
             LayoutInflater inflater = getLayoutInflater();
-            inflater.inflate(R.layout.video_capture, appRoot);
+            //inflater.inflate(R.layout.video_capture, appRoot);
         }
+    }
+
+    private void initWearableConnector() {
+
+        // Setup wearable connection callbacks
+        GoogleApiClient.OnConnectionFailedListener wearableConnectionFailedListener = new GoogleApiClient.OnConnectionFailedListener() {
+            @Override
+            public void onConnectionFailed(ConnectionResult result) {
+                Log.d(TAG, "onConnectionFailed: " + result);
+            }
+        };
+        GoogleApiClient.ConnectionCallbacks wearableConnectionCallbacks = new GoogleApiClient.ConnectionCallbacks() {
+            @Override
+            public void onConnected(Bundle connectionHint) {
+                Log.d(TAG, "onConnected: " + connectionHint);
+                String message = "Hello wearable!";
+                mWearableConnector.sendMessage(message);
+            }
+            @Override
+            public void onConnectionSuspended(int cause) {
+                Log.d(TAG, "onConnectionSuspended: " + cause);
+            }
+        };
+
+        mWearableConnector = new WearableConnector(
+                this,
+                wearableConnectionCallbacks,
+                wearableConnectionFailedListener);
+
+        // Register a local broadcast receiver to handle messages that
+        // have been received by the wearable message listening service.
+        mLocalMessageReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String message = intent.getStringExtra(CamcorderRemoteConstants.MESSAGE_INTENT_EXTRA);
+                Log.d(TAG, "Message received from wearable: " + message);
+                if (message.equals(CamcorderRemoteConstants.REQUEST_RECORD)) {
+                    mWearableConnector.sendMessage(CamcorderRemoteConstants.RESPONSE_RECORDING);
+                }
+                else if(message.equals(CamcorderRemoteConstants.REQUEST_RESUME)) {
+                    mWearableConnector.sendMessage(CamcorderRemoteConstants.RESPONSE_RESUMED);
+                }
+                else if(message.equals(CamcorderRemoteConstants.REQUEST_PAUSE)) {
+                    mWearableConnector.sendMessage(CamcorderRemoteConstants.RESPONSE_PAUSED);
+                }
+                else if(message.equals(CamcorderRemoteConstants.REQUEST_STOP)) {
+                    mWearableConnector.sendMessage(CamcorderRemoteConstants.RESPONSE_STOPPED);
+                }
+
+            }
+        };
+        IntentFilter messageFilter = new IntentFilter(Intent.ACTION_SEND);
+        LocalBroadcastManager.getInstance(this).registerReceiver(mLocalMessageReceiver, messageFilter);
+    }
+
+    private void initVideoRecorder() {
+
+        mOrientationListener = new VideoCaptureOrientationEventListener(this);
+
+        mRecordingStateContext = new RecordingStateContext(mActivity);
+        mRecordingStateContext.updateDisplayedButtons();
+
+        mVideoRecorder = new VideoRecorder();
+        mVideoRecorder.setOnInitCameraListener(new InitCameraListener(){
+            @Override
+            public void onInit() {
+                mCameraPreviewFrame.addView(mVideoRecorder.getCameraPreview(), 0);
+                mRecordingStateContext.changeState(new ReadyRecordingState(mActivity));
+                mRecordingStateContext.updateDisplayedButtons();
+            }
+        });
+        mVideoRecorder.setOnReleaseCameraListener(new ReleaseCameraListener(){
+            @Override
+            public void onRelease() {
+                mCameraPreviewFrame.removeView(mVideoRecorder.getCameraPreview());
+            }
+        });
     }
 }
