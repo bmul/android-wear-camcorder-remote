10ea23c62344d9cb2bcc7270b5da790f821187a9
brian mullins <bmul@users.noreply.github.com>
2014-11-30 11:47:08 -0500

    Fix hard-coded preview sizes. Ran into a problem testing with Android 5.0 device.

--- mobile/src/main/java/com/bcmaffordances/camcorderremote/video/CameraPreview.java
+++ mobile/src/main/java/com/bcmaffordances/camcorderremote/video/CameraPreview.java
@@ -2,7 +2,6 @@ package com.bcmaffordances.camcorderremote.video;
 
 import android.content.Context;
 import android.hardware.Camera;
-import android.media.CamcorderProfile;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -19,12 +18,10 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
     private static final String TAG = "CameraPreview";
 
     private final Camera mCamera;
-    private final CamcorderProfile mCamcorderProfile;
 
     public CameraPreview(Context context, Camera camera) {
         super(context);
         mCamera = camera;
-        mCamcorderProfile = CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH);
         super.getHolder().addCallback(this);
     }
 
@@ -43,11 +40,17 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
     }
 
     public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+
         Log.d(TAG, "surfaceChanged()");
+
         Camera.Parameters parameters = mCamera.getParameters();
-        parameters.setPreviewSize(
-                mCamcorderProfile.videoFrameWidth,
-                mCamcorderProfile.videoFrameHeight);
+        List<Camera.Size> previewSizes = parameters.getSupportedPreviewSizes();
+        if (!previewSizes.isEmpty()) {
+            Camera.Size largestPreviewSize = previewSizes.get(0);
+            parameters.setPreviewSize(largestPreviewSize.width, largestPreviewSize.height);
+        } else {
+            Log.e(TAG, "Camera preview sizes are unavailable");
+        }
         requestLayout();
 
         // The last fps range item will have the greatest max fps.
